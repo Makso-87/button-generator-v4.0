47d94fb6cebbf898053f0318ec69fd3626c0e060
maksimpopov <makso16108@gmail.com>
2024-06-17 17:38:48 +0300

    DEV-5 Добавлено использование контекста для хранения состояния параметров кнопки.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import ContextProvider from './components/ContextProvider/ContextProvider';
 
 const App = () => {
-  return <Layout />;
+  return (
+    <ContextProvider>
+      <Layout />
+    </ContextProvider>
+  );
 };
 
 export default App;
