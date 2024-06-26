7b72a9885b84797b3dc6e23e5cfaa02ce2a7bb40
maksimpopov <makso16108@gmail.com>
2024-06-26 21:35:26 +0300

    DEV-7 Изменена структура состояния приложения, чтобы можно было задавать не только значение CSS свойства, но единицы измерения и возможно другие параметры в будущем.

--- src/components/ContextProvider/ContextProvider.tsx
+++ src/components/ContextProvider/ContextProvider.tsx
@@ -1,20 +1,20 @@
 import React, { ReactNode, useState } from 'react';
-import ButtonGeneratorContext from '../../utils/buttonGeneratorContext';
+import ButtonGeneratorContext, { TCSSPropertiesData } from '../../utils/buttonGeneratorContext';
 import { tabs } from '../../utils/inputsData';
 import { getChangeMethodName } from '../../utils/getChangeMethodName';
 
 export type TContextProviderMethod = (value: string) => void;
 
 const ContextProvider = ({ children }: { children: ReactNode }) => {
-  const initialState: Record<string, string | number> = {};
+  const initialState: Record<string, TCSSPropertiesData> = {};
   const methods: Record<string, TContextProviderMethod> = {};
 
   for (const tab in tabs) {
     tabs[tab].inputs.forEach((item) => {
-      initialState[item.name] = item.defaultValue;
+      initialState[item.name] = { value: item.defaultValue, units: item.units };
 
       methods[getChangeMethodName(item.name)] = (value: string | number) => {
-        setState({ ...state, [item.name]: value });
+        setState({ ...state, [item.name]: { value, units: item.units } });
       };
     });
   }
